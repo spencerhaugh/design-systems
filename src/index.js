80e6b4391fca95b99b3d45216e18ed7be3204ffb
Spencer Haugh <spencer.haugh@gmail.com>
2021-11-02 18:40:18 -0400

    Adding util styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import PrimaryButton from "./components/Buttons";
+import PrimaryButton, { SecondaryButton, TertiaryButton } from "./components/Buttons";
 
-const App = () => <PrimaryButton>Hello world!</PrimaryButton>;
+const App = () => (
+    <div>
+        <PrimaryButton>Hello world!</PrimaryButton>
+        <SecondaryButton>Goodbye World</SecondaryButton>
+        <TertiaryButton>Hey world...</TertiaryButton>
+    </div>
+);
 
 ReactDOM.render(<App />, document.querySelector('#root'));
